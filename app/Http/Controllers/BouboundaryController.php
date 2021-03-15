c97c94fe813fa2339273d9fc6826eb69b195b897
Felipe Almeida <alm.felipe@gmail.com>
2021-03-15 16:19:44 -0300

    reversegeocode

---
+++ app/Http/Controllers/BouboundaryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class BouboundaryController extends Controller
+{
+
+    private function reverseGeocodeQuery($latitude, $longitude){
+        $query = "
+            select
+                boundary.id_region as region_id,
+                region.name as region_name,
+                boundary.id_state as state_id,
+                state.name as state_name,
+                boundary.id_city as city_id,
+                city.name as city_name
+            from
+                ibge.boundary
+                join region on region.id = boundary.id_region
+                join state on state.id = boundary.id_state
+                join city on city.id = boundary.id_city
+            where
+                boundary.id_city is not null and
+                st_contains(
+                    boundary.geometry_shape,
+                    point(:latitude, :longitude)
+                ) = 1
+            limit 1
+        ";
+
+        $reverseGeocode = DB::select(DB::raw($query), ["latitude" => $latitude, "longitude" => $longitude]);
+
+        if(count($reverseGeocode) > 0){
+            return response()->json(['reversegeocode' => $reverseGeocode[0]]);
+        }else{
+            return response()->json(['error' => ['msg' => ['Not found']] ], 404);
+        }
+    }
+
+    public function reverseGeocode(Request $request){
+        $validator = Validator::make($request->all(), [
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 500);
+        }
+
+        return $this->reverseGeocodeQuery($request->input('latitude'), $request->input('longitude'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('reversegeocode', 'App\Http\Controllers\BouboundaryController@reverseGeocode');
+
+//http://localhost:8000/api/reversegeocode?latitude=-12.974722&longitude=-38.476665
+Route::get('reversegeocode', 'App\Http\Controllers\BouboundaryController@reverseGeocode');
