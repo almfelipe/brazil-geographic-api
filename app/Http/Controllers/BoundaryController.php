2377351be02bc4efc69edd2357b570a8230fb3c0
Felipe Almeida <alm.felipe@gmail.com>
2021-03-16 13:11:58 -0300

    fix controller name

--- app/Http/Controllers/BoundaryController.php
+++ app/Http/Controllers/BoundaryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
-class BouboundaryController extends Controller
+class BoundaryController extends Controller
 {
 
     private function reverseGeocodeQuery($latitude, $longitude){
